fbc2fe80e4e33bd2d72c522044f247044442d19e
Marcelo Surco <70810134+marcelo130102@users.noreply.github.com>
2023-02-01 13:14:52 -0500

    Update database.js

--- proyecto/database.js
+++ proyecto/database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'marcelo',
-    password: '1301200224012004Mss',
+    password: '',
     database: 'test'
 })
 
